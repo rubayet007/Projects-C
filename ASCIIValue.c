0149ce86b9f94a4aaae1f5fd2543488c07588777
rubayet007 <83724490+rubayet007@users.noreply.github.com>
2021-11-06 01:58:15 +0600

    Add files via upload

---
+++ ASCIIValue.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+
+{
+    char ch;
+    printf("\nenter any character=");
+    scanf("%c",&ch);
+    printf("\nAscll Value for this character=%d\n",ch);
+
+    int num;
+    printf("\nenter any ASCII Value=");
+    scanf("%d",&num);
+    printf("\n The character for this ASCII value is=%c\n",num);
+
+    return 0;
+}
